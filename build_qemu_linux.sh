2af1b3fc1fe3db9b76f1436d938be988d1d5272e
jinsong <njs145@naver.com>
2025-07-13 15:34:39 +0900

    build script 수정

--- build_qemu_linux.sh
+++ build_qemu_linux.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- defconfig
-echo "The build is running in the background. To check on it, run: tmux attach -t linux_kernel-build"
-echo "To detach and return to your shell, press Ctrl + b, then d."
-tmux new-session -d -s linux_kernel-build -- \
-"ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- make -j$(nproc)"
+#echo "The build is running in the background. To check on it, run: tmux attach -t linux_kernel-build"
+#echo "To detach and return to your shell, press Ctrl + b, then d."
+#tmux new-session -d -s linux_kernel-build -- \
+#"ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- make -j$(nproc)"
+ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- make -j$(nproc)
